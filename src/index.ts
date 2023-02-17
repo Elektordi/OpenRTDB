4fe57d1c1a0aac7d7f5c5a7fc3c8bcc642d90c47
Guillaume Genty <elektordi@elektordi.net>
2023-02-17 19:35:00 +0100

    Saving and loading database and rules from json files

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,62 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import cors from 'cors'
 import express from 'express'
 import * as dotenv from 'dotenv'
 
 
 dotenv.config()
+const api_port = parseInt(process.env.API_PORT || "9000");
+const data_dir = path.resolve(process.env.DATA_DIR || (__dirname + "/../data"));
+const flush_interval = parseInt(process.env.FLUSH_INTERVAL || "60");
+
+if(!process.env.DATA_DIR && !fs.existsSync(data_dir)) {
+    console.log(`${data_dir} did not exist and was created!`)
+    fs.mkdirSync(data_dir);
+}
+
 export const app = express()
 let database: any = {}
+let database_changed: boolean = false;
+let rules: any = {}
+
+const database_path = path.resolve(data_dir, "database.json")
+console.log(database_path);
+if(fs.existsSync(database_path)) {
+    database = JSON.parse(fs.readFileSync(database_path, 'utf8'))
+    console.log(`Database loaded from ${database_path}`)
+} else {
+    console.log("Empty database created!")
+}
+
+const rules_path = path.resolve(data_dir, "rules.json")
+if(fs.existsSync(rules_path)) {
+    rules = JSON.parse(fs.readFileSync(rules_path, 'utf8'))
+    console.log(`Rules loaded from ${rules_path}`)
+} else {
+    console.log("Empty rules created!")
+}
+
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.json({ "Hello": "World" });
-})
+app.route("/")
+    .get((req, res) => {
+        res.json({ "Hello": "World" });
+    })
+
+app.route("/~rules")
+    .get((req, res) => {
+        res.json(rules);
+    })
+    .post((req, res) => {
+        rules = req.body;
+        fs.writeFile(rules_path, JSON.stringify(rules), (err) => {
+            if (err) console.log(`Failed to save rules to ${rules_path} : ${err}`);
+            else res.status(204).send();
+        });
+    })
 
 app.route("/*.json")
     .get((req, res) => {
@@ -34,6 +78,7 @@ app.route("/*.json")
         let data = req.body;
         if(path.length == 0) {
             database = data;
+            database_changed = true;
             res.json(data);
             return;
         }
@@ -46,11 +91,19 @@ app.route("/*.json")
             dbref = dbref[elem];
         }
         dbref[path[0]] = data;
+        database_changed = true;
         res.json(data);
     })
 
-const api_port = process.env.API_PORT || 9000;
 app.listen(api_port, () => {
     console.log(`API server running: http://127.0.0.1:${api_port}`)
 })
 
+setInterval(() => {
+    if(!database_changed) return;
+    console.log("Saving database...");
+    fs.writeFile(database_path, JSON.stringify(database), (err) => {
+        if (err) console.log(`Failed to save database to ${database_path} : ${err}`);
+        else console.log(`Database saved to ${database_path}`);
+    });
+}, flush_interval*1000)
